4960a903e9ec12692e3ca727755dfbe8133b0df3
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-02-23 12:08:19 +0500

    class3

---
+++ templates/mean.js
@@ -0,0 +1,8 @@
+var shirt = 7200;
+console.log(shirt);
+var hooria = "hi";
+console.log(hooria);
+//boolean
+var frame = 'fatima';
+var name1 = 'hooria';
+console.log("".concat(frame).concat(name1));
---
+++ templates/mean.ts
@@ -0,0 +1,13 @@
+let shirt: number =7200
+console.log(shirt)
+let hooria: string ="hi"
+console.log(hooria)
+
+//boolean
+let isold: boolean =false
+console.log(isold)
+
+
+let frame : string ='fatima' 
+let name1 : string ='hooria'
+console.log(`${frame}${name1}`)
