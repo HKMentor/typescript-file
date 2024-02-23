90f2a9d991ec172a9ffefe02b8d3eace6c3b8f84
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-02-23 12:11:43 +0500

    class5

---
+++ IF&ELSE statement/mean.js
@@ -0,0 +1,25 @@
+"use strict";
+//if_else
+let gate = 'fatima';
+if (gate === 'fatima') {
+    console.log('hello,you can come!');
+}
+else if (gate === 'moiz') {
+    console.log('hello you come !moiz');
+}
+else {
+    console.log('no you cant come');
+}
+//ticketting system
+let country = 'pakistan';
+let age = 18;
+if (country === "pakistan")
+    if (age <= 18) {
+        console.log("here is your ticket");
+    }
+    else {
+        console.log("age registration");
+    }
+else {
+    console.log("invalid country");
+}
