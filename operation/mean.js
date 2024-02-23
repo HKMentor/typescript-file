202c3b121334c079be56c3510574ba76152ee02b
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-02-23 12:09:49 +0500

    class4

---
+++ operation/mean.js
@@ -0,0 +1,21 @@
+//operator 
+//airthmeticoperator
+//addition
+var number1 = 1;
+var number2 = 2;
+console.log(number1 + number2);
+var n1 = "hello";
+var n2 = "hi";
+console.log(n1 + " " + n2);
+//subtraction
+var no1 = 9;
+var no2 = 5;
+console.log(no1 - no2);
+//multiply
+var num1 = 3;
+var num2 = 3;
+console.log(num1 * num2);
+//divison
+var num01 = 6;
+var num02 = 2;
+console.log(num01 / num02);
---
+++ operation/mean.ts
@@ -0,0 +1,32 @@
+//operator 
+//airthmeticoperator
+
+//addition
+ let number1= 1
+ let number2= 2
+ console.log (number1 + number2)
+
+ let n1= "hello"
+ let n2= "hi"
+ console.log(n1+" "+n2)
+
+ //subtraction
+ let no1= 9
+ let no2= 5
+console.log(no1-no2)
+
+//multiply
+let num1= 3
+let num2= 3
+console.log(num1 * num2)
+
+
+//divison
+let num01= 6
+let num02= 2
+console.log(num01/num02)
+
+//exponentationt 
+let nu1= 2
+let nu2= 2
+console.log(nu1**nu2)
